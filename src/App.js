58b756d9034e368afd2067140927fe71782757ac
nik <nicola.gnecchi@gmail.com>
2021-03-25 08:58:50 +0100

    share images and hashed navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Router } from "wouter";
 
 import Manifest from "./components/Manifest";
 import City from "./components/City";
 
+const currentLocation = () => {
+  return window.location.hash.replace(/^#/, "") || "/";
+};
+
+const navigate = (to) => (window.location.hash = to);
+
+const useHashLocation = () => {
+  const [loc, setLoc] = useState(currentLocation());
+  useEffect(() => {
+    const handler = () => setLoc(currentLocation());
+    window.addEventListener("hashchange", handler);
+    return () => window.removeEventListener("hashchange", handler);
+  }, []);
+  return [loc, navigate];
+};
+
 function App() {
   return (
     <div id="app">
-      <Router base="/whatwebreathe">
+      <Router hook={useHashLocation} base="/whatwebreathe">
         <Switch>
           <Route path="/manifest">
             <Manifest />
